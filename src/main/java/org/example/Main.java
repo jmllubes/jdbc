ce42862afd0d2ec4a732c2def6afc640e470de3f
Josep Maria Llubes Cliville <jmllubes@gmail.com>
2023-12-18 15:31:47 +0100

    insert

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -18,16 +18,33 @@ public class Main {
         Statement stmt = conn.createStatement();
         Scanner sc = new Scanner(System.in);
         System.out.println("Introdueix la opció que vulguis: ");
-        System.out.println("a. Afegir un nou bomber");
-        System.out.println("b. Mostrar tots els bombers");
-        System.out.println("c. Eliminar un bomber");
-        System.out.println("d. Modificar un bomber");
+        System.out.println("a. Afegir un nou bomber"); // INSERT
+        System.out.println("b. Mostrar tots els bombers"); // SELECT
+        System.out.println("c. Eliminar un bomber"); // DELETE
+        System.out.println("d. Modificar un bomber"); // UPDATE
         System.out.println("e. Sortir");
         String lletra = sc.nextLine();
 
         while(!lletra.equals("e")){
             if(lletra.equals("a")){
+                System.out.println("Introdueix el parque_id del bomber 1 o 2: ");
+                int parque_id = sc.nextInt();//1
+                sc.nextLine();// salt de linia
+                System.out.println("Introdueix els apellidos del bomber: ");
+                String apellidos = sc.nextLine(); // hola + enter
                 System.out.println("Introdueix el nom del bomber: ");
+                String nombre = sc.nextLine();
+                System.out.println("Introdueix el dni del bomber: ");
+                String dni = sc.nextLine();
+                System.out.println("Introdueix la data de naixement del bomber: ");
+                String fecha_nacimiento = sc.nextLine();
+                System.out.println("Introdueix la direcció del bomber: ");
+                String direccion = sc.nextLine();
+                System.out.println("Introdueix el telèfon del bomber: ");
+                String telefono = sc.nextLine();
+                String sql = "INSERT INTO `bombero`(`parque_id`, `apellidos`, `nombre`, `dni`, `fecha_nacimiento`, `direccion`, `telefono`) VALUES ("+parque_id+",'"+apellidos+"','"+nombre+"','"+dni+"','"+fecha_nacimiento+"','"+direccion+"','"+telefono+"')";
+                int rows = stmt.executeUpdate(sql);
+                System.out.println("Numero de files afectades: " + rows);
             }
             else if(lletra.equals("b")) {
                 String sql = "SELECT `id`, `parque_id`, `apellidos`, `nombre`, `dni`, `fecha_nacimiento`, `direccion`, `telefono` FROM `bombero`";
